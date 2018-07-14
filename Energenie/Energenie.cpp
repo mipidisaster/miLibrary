bd2513da3eb866e22b7107e3a50baf78f826ce5e
= <elack123@my.com>
2018-07-14 10:54:34 +0100

    Quick class of Energenie put together

---
+++ Energenie/Energenie.cpp
@@ -0,0 +1,69 @@
+/**************************************************************************************************
+ * @file        Energenie.cpp
+ * @author      Thomas
+ * @version     V0.1
+ * @date        14 Jul 2018
+ * @brief       << Manually Entered >>
+ **************************************************************************************************
+ @ attention
+
+ << To be Introduced >>
+
+ *************************************************************************************************/
+#include <Energenie/Energenie.h>
+
+Energenie::Energenie()
+{
+    this->ModulationEnab    = new GPIO(GPIO_LOW, 25, GPIO_OUT); // Setup Modulation pin (LOW)
+    this->ASKSelector       = new GPIO(GPIO_LOW, 24, GPIO_OUT); // Setup ASKselector (LOW)
+
+    this->D0                = new GPIO(GPIO_LOW, 17, GPIO_OUT); // Setup D0
+    this->D1                = new GPIO(GPIO_LOW, 22, GPIO_OUT); // Setup D1
+    this->D2                = new GPIO(GPIO_LOW, 23, GPIO_OUT); // Setup D2
+    this->D3                = new GPIO(GPIO_LOW, 27, GPIO_OUT); // Setup D3
+}
+
+void Energenie::SwitchOn(void)
+{
+    this->D0->setValue(GPIO_HIGH);  //  Setup for socket 1 selection
+    this->D1->setValue(GPIO_HIGH);  //
+    this->D2->setValue(GPIO_HIGH);  //
+    this->D3->setValue(GPIO_HIGH);  //  Switch ON
+
+    delay(100);     // Settle time for encoder
+
+    this->ModulationEnab->setValue(GPIO_HIGH);   // Select Modulation
+
+    delay(250);    // Keep enabled for a period
+
+    this->ModulationEnab->setValue(GPIO_LOW);  // DeSelect
+}
+
+void Energenie::SwitchOff(void)
+{
+    this->D0->setValue(GPIO_HIGH);  //  Setup for socket 1 selection
+    this->D1->setValue(GPIO_HIGH);  //
+    this->D2->setValue(GPIO_HIGH);  //
+    this->D3->setValue(GPIO_LOW);   //  SWITCH OFF
+
+    delay(100);     // Settle time for encoder
+
+    this->ModulationEnab->setValue(GPIO_HIGH);   // Select Modulation
+
+    delay(250);    // Keep enabled for a period
+
+    this->ModulationEnab->setValue(GPIO_LOW);  // DeSelect
+}
+
+
+Energenie::~Energenie()
+{
+    delete [] ModulationEnab;               // Delete the array "ModulationEnab"
+    delete [] ASKSelector;               // Delete the array "ASKSelector"
+
+    delete [] D0;               // Delete the array "D0"
+    delete [] D1;               // Delete the array "D1"
+    delete [] D2;               // Delete the array "D2"
+    delete [] D3;               // Delete the array "D3"
+}
+
---
+++ Energenie/Energenie.h
@@ -0,0 +1,64 @@
+/**************************************************************************************************
+ * @file        Energenie.h
+ * @author      Thomas
+ * @version     V0.1
+ * @date        14 Jul 2018
+ * @brief       << Manually Entered >>
+ **************************************************************************************************
+ @ attention
+
+ << To be Introduced >>
+
+ *************************************************************************************************/
+/**************************************************************************************************
+ * How to use
+ * ----------
+ * 
+ * 
+ *************************************************************************************************/
+#ifndef ENERGENIE_H_
+#define ENERGENIE_H_
+
+#include "GPIO/GPIO.h"
+#include <wiringPi.h>
+
+#if   defined(zz__MiSTM32Fx__zz)        // If the target device is an STM32Fxx from cubeMX then
+//==================================================================================================
+#error "Not supported"
+
+#elif defined(zz__MiRaspbPi__zz)        // If the target device is an Raspberry Pi then
+//==================================================================================================
+// Add includes specific to the Raspberry Pi
+
+#else
+//==================================================================================================
+#error "Unrecognised target device"
+
+#endif
+
+// Defines specific within this class
+//	--- Include any #defines within class - use similar naming as class
+//	--- keep this relatively light, large quantities should be within a seperate header file
+//	--- named <class_name>_defs.h
+
+// Types used within this class
+//	--- Include an types used within this class, put "None" if not required
+
+class Energenie {
+private:
+    GPIO    *ModulationEnab;        // Pointer to the GPIO to enable/disable modulation
+    GPIO    *ASKSelector;           // Pointer to the GPIO to select ASK/FSK
+
+    GPIO    *D0;                    // Pointer to D0
+    GPIO    *D1;                    // Pointer to D1
+    GPIO    *D2;                    // Pointer to D2
+    GPIO    *D3;                    // Pointer to D3
+
+public:
+    Energenie();
+    void SwitchOn();
+    void SwitchOff();
+    virtual ~Energenie();
+};
+
+#endif /* ENERGENIE_H_ */
