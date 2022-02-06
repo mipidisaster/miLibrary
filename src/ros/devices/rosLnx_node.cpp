196e6e27f8679687e26c701bf6205098e0ec082c
Thomas Carrington <thomasx476@googlemail.com>
2022-02-06 17:02:10 +0000

    Ref issue #17 step 3, ROS node created for this class. Has been tested as works as expected. New message introduced specific for processor nodes, so can see how each core is doing loading wise

---
+++ src/ros/devices/rosLnx_node.cpp
@@ -0,0 +1,276 @@
+/**************************************************************************************************
+ * @file        rosLnx_node.cpp
+ * @author      Thomas
+ * @brief       ROS node for the LnxCond class
+ **************************************************************************************************
+  @ attention
+
+  << To be Introduced >>
+
+ *************************************************************************************************/
+/**************************************************************************************************
+ * How to use
+ * ----------
+ * This node is called "lnx_node"
+ *   Configuration parameters:
+ *      ~/config        /loop_rate
+ *                      [ Floating data value, specifying the rate that the Linux device is
+ *                      [ interrogated.
+ *                      [ - Note, if none is provided will default to '4Hz'
+ *
+ *   Publishers:
+ *      CPUTemp
+ *                      [ Provides the temperature reading of the processor with embedded linux
+ *
+ *      CPULoad
+ *                      [ Provides the processor load of the embedded linux device, providing the
+ *                      [ overal percentage load as well as individual cores
+ *                      [ Uses milibrary/msg/ProcessorUtilisation.msg
+ *
+ *   Subscribers:
+ *      None
+ *
+ *   Services:
+ *      None
+ *
+ *************************************************************************************************/
+// C System Header(s)
+// ------------------
+#include <stdint.h>
+#include <vector>       // for std::vector
+#include <string>       // for strings
+
+// C++ System Header(s)
+// --------------------
+// None
+
+// Other Libraries
+// ---------------
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+
+#include <std_msgs/Float32.h>
+
+// Project Libraries
+// -----------------
+#include "milibrary/ProcessorUtilisation.h"
+
+#include "FileIndex.h"
+// ~~~~~~~~~~~~~~~~~~~
+#include FilInd_LnxCondHD               // Header for Linux Condition
+
+//=================================================================================================
+/**************************************************************************************************
+ * Define any private variables
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *************************************************************************************************/
+// None
+
+/**************************************************************************************************
+ * Define any private function prototypes
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *************************************************************************************************/
+class rosLnx {
+private:
+    std::string         kconfig_sub_area            = "config/";
+
+    std::string         knode_loop_rate             = "loop_rate";
+
+    std::string         kLnxCond_publish_temp       = "CPUTemp";
+    std::string         kLnxCond_publish_cores      = "CPULoad";
+
+private:
+    LnxCond                 *_hardware_handle_  = NULL;
+
+    std::vector<float>      _cpu_cores_;
+
+/**************************************************************************************************
+ * == ROS STUFF == >>>   ROBOT OPERATING SYSTEM (ROS) OBJECTS    <<<
+ *   -----------
+ *  The follow are objects used for interfacing with the Robot Operating System.
+ *************************************************************************************************/
+    ros::NodeHandle         _nh_;
+    ros::NodeHandle         _private_nh_;
+
+    ros::NodeHandle         _nh_hardware_;
+    ros::CallbackQueue      _hardware_callback_queue_;
+
+    // PARAMETERS
+    ////////////////////////
+    double                          _loop_rate_parameter_;
+
+    // MESSAGES
+    ////////////////////////
+    std_msgs::Float32               _cpu_temperature_message_;
+    milibrary::ProcessorUtilisation _cpu_utilisation_message_;
+
+    // PUBLISHERS
+    ////////////////////////
+    ros::Publisher                  _cpu_temperature_publisher_;
+    ros::Publisher                  _cpu_utilisation_publisher_;
+
+    // SUBSCRIBERS
+    ////////////////////////
+
+    // TIMERS
+    ////////////////////////
+
+    // SERVICES
+    ////////////////////////
+
+    // ACTIONS
+    ////////////////////////
+
+public:
+/*
+ *  @brief:  Create the LnxCond node class, basic construction
+ *
+ *  @param:  Pointer to the 'NodeHandle'
+ *  @param:  Pointer to the 'NodeHandle', setup for private - ("~")
+ *  @retval: rosLnx class
+ */
+    rosLnx(ros::NodeHandle* normal, ros::NodeHandle* private_namespace):
+    _nh_(*normal), _private_nh_(*private_namespace)
+    {
+        //_nh_hardware_.setCallbackQueue(&_hardware_callback_queue_);
+
+        if (configNode() < 0) {
+            ROS_ERROR("Error detected during LnxCond construction, exiting node...");
+            return;
+        }
+
+        nodeLoop();
+    }
+
+    /*
+     *  @brief:  Separate function, to handle the hardware service callback queue.
+     *           Intended to be used within a dedicated thread.
+     *
+     *  @param:  void
+     *  @retval: void
+     */
+    void hardwareCallbackThread(void) {
+        ros::SingleThreadedSpinner spinner;
+        spinner.spin(&_hardware_callback_queue_);
+    }
+
+    /*
+     *  @brief:  Function to encapsulate the looping of this node.
+     *
+     *  @param:  void
+     *  @retval: void
+     */
+    void nodeLoop(void) {
+        ROS_INFO("LnxCond node ready for use");
+
+        ros::Rate loop_rate(_loop_rate_parameter_);
+
+        _cpu_temperature_message_.data = -999;
+        _cpu_cores_.insert( _cpu_cores_.begin(), LNX_NUM_CORES, -1.00f  );
+
+        while (ros::ok()) {
+            if (_hardware_handle_->updateStatus() != LnxCond::DevFlt::kNone) {
+                _cpu_temperature_message_.data      = -999;
+                _cpu_utilisation_message_.overall   = -1;
+                _cpu_utilisation_message_.cores     = _cpu_cores_;
+            }
+            else {
+                _cpu_temperature_message_.data      = _hardware_handle_->temperature;
+                _cpu_utilisation_message_.overall   = (_hardware_handle_->cpu_load[0] * 100);
+
+                for (int i = 0; i != _cpu_cores_.size(); i ++) {
+                    _cpu_cores_[i] = (_hardware_handle_->cpu_load[i + 1] * 100);
+                }
+                _cpu_utilisation_message_.cores     = _cpu_cores_;
+
+            }
+
+            _cpu_temperature_publisher_.publish(_cpu_temperature_message_);
+            _cpu_utilisation_publisher_.publish(_cpu_utilisation_message_);
+
+
+            ros::spinOnce();
+            loop_rate.sleep();
+        }
+    }
+
+    /*
+     *  @brief:  Setups the LnxCond for the node, as per the expected input/configuration
+     *           parameters from within the rosparam space.
+     *           If there are any issues with the supplied values; which cannot be managed
+     *           internally. Will return an error (value of -1), to be checked at the 'main' level
+     *           to close the node down.
+     *
+     *  @param:  void
+     *  @retval: Integer value - 0 = no issues, -1 = issues detected
+     */
+    int configNode(void)
+    {
+        //=========================================================================================
+        // Input parameters
+        _private_nh_.param<double>(kconfig_sub_area + knode_loop_rate,
+                                   _loop_rate_parameter_,
+                                   4.0);
+
+        //=========================================================================================
+        // Duplication check
+
+        //=========================================================================================
+
+        _hardware_handle_ = new LnxCond();
+
+        ROS_INFO("LnxCond has been setup");
+        //=========================================================================================
+        // Publishers
+        _cpu_temperature_publisher_   = _nh_.advertise<std_msgs::Float32>(
+                                                kLnxCond_publish_temp,
+                                                5);
+
+        _cpu_utilisation_publisher_   = _nh_.advertise<milibrary::ProcessorUtilisation>(
+                                                kLnxCond_publish_cores,
+                                                5);
+
+        //=========================================================================================
+        // Timers
+
+        //=========================================================================================
+        // Clients/Servers
+
+        //=========================================================================================
+
+        ROS_INFO("LnxCond node constructed");
+        return 0;   // If got to this point, no errors were detected
+    }
+
+    ~rosLnx() {
+        ROS_INFO("Shutting down the node, and killing functions");
+        delete _hardware_handle_;
+    }
+
+};
+
+//=================================================================================================
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "linux_basic");
+    ros::NodeHandle n;
+    ros::NodeHandle private_params("~");
+
+    rosLnx  node(&n, &private_params);
+
+    ros::waitForShutdown();
+
+    // On node shutdown, don't think it reaches this part of main()
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/**************************************************************************************************
+ * ===============================================================================================
+ * Local functions
+ * ===============================================================================================
+ *************************************************************************************************/
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// None
+
